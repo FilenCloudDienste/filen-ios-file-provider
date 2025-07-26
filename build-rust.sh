36dab3099dd117dfd04c1044d29bab8143d615d3
Enduriel <endur1el@protonmail.com>
2025-07-26 13:50:58 +0200

    fix: incorrect frameworks path in build script

--- build-rust.sh
+++ build-rust.sh
@@ -48,5 +48,5 @@ xcodebuild -create-xcframework \
 	-library target/aarch64-apple-ios-sim/release/lib${libname}.a -headers target/uniffi-xcframework-staging \
 	-output target/ios/lib${libname}.xcframework
 
-cp -r target/ios/lib${libname}.xcframework "$xcode_dir/Frameworks/lib${libname}.xcframework"
+cp -r target/ios/lib${libname}.xcframework "$xcode_dir/Frameworks"
 cp target/uniffi-xcframework-staging/${libname}.swift "$xcode_dir/FilenFileProviderExtension/filen_mobile_native_cache.swift"
